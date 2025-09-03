d6254fa996e5b042d0ec0ecc719d6bb17a1d89d1
Hearly98 <shuertas981@gmail.com>
2025-09-02 23:03:18 -0500

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,32 +8,6 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          50: '#E3F2FD',
-          100: '#BBDEFB',
-          200: '#90CAF9',
-          300: '#64B5F6',
-          400: '#42A5F5',
-          500: '#2196F3', // base
-          600: '#1E88E5',
-          700: '#1976D2',
-          800: '#1565C0',
-          900: '#0D47A1',
-        },
-        secondary: {
-          50: '#FFF8E1',
-          100: '#FFECB3',
-          200: '#FFE082',
-          300: '#FFD54F',
-          400: '#FFCA28',
-          500: '#FFC107', // base
-          600: '#FFB300',
-          700: '#FFA000',
-          800: '#FF8F00',
-          900: '#FF6F00',
-        },
-      },
       screens: {
         sm: "640px",
         md: "768px",
