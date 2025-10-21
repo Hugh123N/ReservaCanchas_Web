ff566e1d8fd8c5bec843ef6e8e89c6a0eae3ef8c
Hearly98 <shuertas981@gmail.com>
2025-10-20 22:52:52 -0500

    update angular 19 a 20 1V

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,16 +1,11 @@
+import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideServerRouting } from '@angular/ssr';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    //provideNoopAnimations(), //deshabilita las animaciones en el servidor
-    provideServerRouting(serverRoutes)
-  ]
+  providers: [provideServerRendering(withRoutes(serverRoutes))]
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
